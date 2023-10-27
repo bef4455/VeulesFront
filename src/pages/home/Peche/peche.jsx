d546c2bea2cbcfe07b0fbffceef7f825b1ce8bc7
bef4 <befacool@gmail.com>
2023-10-27 17:53:47 +0200

    add peche part

--- src/pages/home/Peche/peche.jsx
+++ src/pages/home/Peche/peche.jsx
@@ -5,10 +5,27 @@ function Peche() {
   return (
     <div>
       <header className="banner">
-        <img src="images/peche-image.jpg" alt="Bannière de pêche" />
+        <img
+          src="https://raw.githubusercontent.com/bef4455/VeulesFront/main/public/IMG_7079.jpeg"
+          alt="Bannière de pêche"
+        />
       </header>
-
-      {/* Le reste du contenu de votre page */}
+      <h1 className="titre">
+        Horaires des marées à Veules les Roses en Normandie
+      </h1>
+      <div className="maree-container">
+        <iframe
+          src="http://www.horaire-maree.fr/widget_complexe.php?port=Veules-les-Roses&allowed=true"
+          width="260"
+          height="260"
+          frameborder="0"
+          scrolling="no"
+        >
+          <a href="http://maree.info/" target="_top" rel="dofollow">
+            Calendrier des marées - horaire, hauteur, coefficient de marée
+          </a>
+        </iframe>
+      </div>
     </div>
   );
 }
