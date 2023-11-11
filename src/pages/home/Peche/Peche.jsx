e21d6f2c81d2cdfc8f11f45d39e89334469e01cb
bef4 <befacool@gmail.com>
2023-11-11 19:03:42 +0100

    add maree

--- src/pages/home/Peche/Peche.jsx
+++ src/pages/home/Peche/Peche.jsx
@@ -11,18 +11,27 @@ function Peche() {
         />
       </header>
       <h1 className="titre">Horaires des marées</h1>
-      <div className="maree-container">
+
+      <div>
         <iframe
-          src="http://www.horaire-maree.fr/widget_complexe.php?port=Veules-les-Roses&allowed=true"
-          width="260"
-          height="260"
-          frameborder="0"
-          scrolling="no"
-        >
-          <a href="http://maree.info/" target="_top" rel="dofollow">
-            Calendrier des marées - horaire, hauteur, coefficient de marée
-          </a>
-        </iframe>
+          src="http://www.horaire-maree.fr/maree/Veules-les-Roses/"
+          width="100%;"
+          height="900px;"
+        ></iframe>
+      </div>
+
+      <div className="description">
+        <p>
+          La pêche à la grise, c’est grisant. <br /> La crevette n’est pas loin
+          quand l’eau n’est pas trop glacée.
+          <br /> Alors que les flots laissent place au sable, choisi ton
+          pousseux et file au devant de la petite grise. <br /> Les pieds dans
+          l’eau, le nez au vent, tes pensées rejoignent les cieux et l’esprit
+          prend congé…
+        </p>
+      </div>
+      <div className="Photo">
+        <img src="./../../../" alt="Bannière de pêche" />
       </div>
     </div>
   );
