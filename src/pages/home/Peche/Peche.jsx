6c7ea5a077911453b8598f3cfb698df8148535a3
bef4 <befacool@gmail.com>
2023-11-12 10:51:13 +0100

    add slide for peche part

--- src/pages/home/Peche/Peche.jsx
+++ src/pages/home/Peche/Peche.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-import "./peche.css";
+import Slider from "react-slick";
+import "./Peche.css";
 
 function Peche() {
+  const photos = [
+    "https://raw.githubusercontent.com/bef4455/VeulesFront/main/public/IMG_2991.jpg",
+    "https://raw.githubusercontent.com/bef4455/VeulesFront/main/public/IMG_2994.JPG",
+    "https://raw.githubusercontent.com/bef4455/VeulesFront/main/public/IMG_2997.jpg",
+    "https://raw.githubusercontent.com/bef4455/VeulesFront/main/public/IMG_3006.jpg",
+    "https://raw.githubusercontent.com/bef4455/VeulesFront/main/public/IMG_3012.jpg",
+    "https://raw.githubusercontent.com/bef4455/VeulesFront/main/public/IMG_8692.jpg",
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div>
       <header className="banner">
@@ -10,16 +28,17 @@ function Peche() {
           alt="Bannière de pêche"
         />
       </header>
-      <h1 className="titre">Horaires des marées</h1>
+      <h1 className="titre">Horaires des marées de Veules les Roses</h1>
 
-      <div>
+      <div className="maree">
         <iframe
           src="http://www.horaire-maree.fr/maree/Veules-les-Roses/"
-          width="100%;"
-          height="900px;"
+          width="100%"
+          height="900px"
+          frameBorder="0"
+          scrolling="no"
         ></iframe>
       </div>
-
       <div className="description">
         <p>
           La pêche à la grise, c’est grisant. <br /> La crevette n’est pas loin
@@ -30,9 +49,13 @@ function Peche() {
           prend congé…
         </p>
       </div>
-      <div className="Photo">
-        <img src="./../../../" alt="Bannière de pêche" />
-      </div>
+      <Slider {...settings}>
+        {photos.map((photo, index) => (
+          <div key={index} className="photo">
+            <img src={photo} alt={`Photo ${index + 1}`} />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
